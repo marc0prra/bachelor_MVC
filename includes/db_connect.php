625d0de018c21a3c039534cc40ca33e38cf387a7
Marco Pereira <pereira.marco2006@gmail.com>
2026-09-11 14:56:08 +0200

    connexion a la abse de donnée

---
+++ includes/db_connect.php
@@ -0,0 +1,17 @@
+<?php
+
+function connection(){
+    $serveur = "loaclhost";
+    $utilisateur = "root";
+    $mdp = "";
+    $bdd = "bachelorMVC";
+
+    try {
+        $connexion = new PDO("mysql:host=$serveur;dbname=$bdd", $utilisateur, $mdp);
+        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $connexion;
+    } catch (PDOException $e) {
+        die("Echec de la connexion : " .  $e->getMessage());
+    }
+}
